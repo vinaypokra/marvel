471f02f287e5c726e31197b7f5c8243d1d18a873
vinaypokra <79596191+vinaypokra@users.noreply.github.com>
2021-03-02 18:47:06 +0530

    commit by vinay

---
+++ src/Services.jsx
@@ -0,0 +1,11 @@
+let colors = {
+  primary: "#4595A4",
+  secondary: "#fff",
+  transparent: "#ffffff00",
+  disableButtonColor: "#ffffff85",
+  err: "red",
+  grey: "#aaa",
+  translucentBG: "RGBA(0,0,0,.3)",
+};
+
+export { colors };
---
+++ src/app-styles.jsx
@@ -0,0 +1,170 @@
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { colors } from "./Services";
+
+const ColorButton = withStyles((theme) => ({
+  root: {
+    color: theme.palette.getContrastText(colors.primary),
+    backgroundColor: colors.primary,
+    border: `1px solid ${colors.primary}`,
+    "&:hover": {
+      backgroundColor: colors.transparent,
+      border: `1px solid white`,
+    },
+  },
+}))(Button);
+
+const handleScroll = () => {
+  window.scroll({
+    top: 350,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+export default {
+  handleScroll,
+  ColorButton,
+  cardRadius: {
+    borderRadius: "5px",
+  },
+  colorWhite: {
+    color: colors.secondary,
+  },
+  colorPrimary: {
+    color: colors.primary,
+  },
+  colorGrey: {
+    color: colors.grey,
+  },
+  blackBG: {
+    background: "#000",
+  },
+  boldTxt: {
+    fontStyle: "bold",
+  },
+  feildRadius: {
+    borderRadius: "5px",
+  },
+  greyBG: {
+    background: "#111",
+  },
+  italicTxt: {
+    fontStyle: "italic",
+  },
+  marginTop: {
+    marginTop: "10px",
+  },
+  padding5: {
+    padding: "5%",
+  },
+  padding1: {
+    padding: "1%",
+  },
+  paddingLR: {
+    paddingLeft: "5%",
+    paddingRight: "5%",
+  },
+  paddingRight5: {
+    paddingRight: "5px",
+  },
+  spacingVertical: {
+    padding: "100px 0",
+  },
+
+  leftTxt: {
+    textAlign: "left",
+  },
+
+  centerTxt: {
+    textAlign: "center",
+  },
+  banner: {
+    paddingTop: "40px",
+    minHeight: "100vh",
+    zIndex: 1000,
+    overflow: "hidden",
+  },
+  deafultButton: {
+    padding: "5px 20px",
+    marginTop: "20px",
+  },
+
+  fixed: {
+    position: "sticky",
+    top: 0,
+  },
+  section01: {
+    padding: "5%",
+    color: colors.secondary,
+    minHeight: "100vh",
+    zIndex: 500,
+    background: "#111",
+  },
+  section01Content: {
+    background: "#000",
+    padding: "5%",
+    textAlign: "center",
+  },
+  marginBottom: {
+    marginBottom: "50px",
+  },
+  section02: {
+    background: "#111",
+    padding: "0 5%",
+    minHeight: "100vh",
+  },
+  translucentContainer: {
+    background: colors.translucentBG,
+    padding: "20px",
+  },
+  fullHt: {
+    minHeight: "100vh",
+    background: "#111",
+  },
+  fullHtNoBG: { height: "100vh" },
+  highZ: {
+    zIndex: 10000,
+  },
+  imgWidth: {
+    width: "100%",
+  },
+  header: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    padding: "15px",
+    background: "rgba(0,0,0,.5)",
+    zIndex: 20000,
+  },
+  popup: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    height: "100vh",
+    paddingTop: "120px",
+    width: "100%",
+    background: "#000",
+  },
+  footer: {
+    padding: "50px",
+    background: "#000",
+    textAlign: "center",
+  },
+  spacing: (value) => `${value * 8}px`,
+  //form styles
+  formFieldContainer: {
+    height: "85px",
+    overflow: "visible",
+    marginBottom: "10px",
+  },
+  verticalScroll: {
+    overflowY: "auto",
+    overflowX: "hidden",
+    maxHeight: "280px",
+  },
+  whiteColor: { color: colors.secondary },
+  err: { color: colors.err },
+  special: { display: "inline-block", width: "5%", textAlign: "center" },
+  countryContainer: { display: "inline-block", width: "15%" },
+  mobileContainer: { display: "inline-block", width: "75%" },
+};
