5ae0404e0c3d15308dfde9675af9e00890646ff3
vinaypokra <79596191+vinaypokra@users.noreply.github.com>
2021-03-03 03:23:21 +0530

    commited by vinay

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import LoveUs from "./Section/landingPage/LoveUs";
-import HowItWorks from "./Section/landingPage/HowItWorks";
+
 import FitnessPlan from "./Section/landingPage/FitnessPlan";
 import Banner from "./Section/landingPage/Banner";
 const Home = () => {
@@ -8,7 +8,7 @@ const Home = () => {
     <>
       <Banner />
       <LoveUs />
-      <HowItWorks />
+
       <FitnessPlan />
     </>
   );
